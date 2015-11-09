ba9f057d3b27b8a87fa93c7b5ce14db963b4e438
Eric Lamb <eric@mithra62.com>
2015-11-09 01:52:14 -0800

    Fixes S3 storage form

--- wp-content/plugins/backup_pro/admin/views/storage/drivers/_s3.php
+++ wp-content/plugins/backup_pro/admin/views/storage/drivers/_s3.php
@@ -1,26 +1,52 @@
 <input type="hidden" value="0" name="s3_reduced_redundancy" />
-<?php 	
-$this->table->add_row(
-    '<label for="s3_access_key">'.$view_helper->m62Lang('s3_access_key').'</label><div class="subtext">'.$view_helper->m62Lang('s3_access_key_instructions').'</div>', 
-    form_input('s3_access_key', $form_data['s3_access_key'], 'id="s3_access_key"').m62_form_errors($form_errors['s3_access_key'])
-);
-
-$this->table->add_row(
-    '<label for="s3_secret_key">'.$view_helper->m62Lang('s3_secret_key').'</label><div class="subtext">'.$view_helper->m62Lang('s3_secret_key_instructions').'</div>', 
-    form_password('s3_secret_key', $form_data['s3_secret_key'], 'id="s3_secret_key"').m62_form_errors($form_errors['s3_secret_key'])
-);
-
-$this->table->add_row(
-    '<label for="s3_bucket">'.$view_helper->m62Lang('s3_bucket').'</label><div class="subtext">'.$view_helper->m62Lang('s3_bucket_instructions').'</div>', 
-    form_input('s3_bucket', $form_data['s3_bucket'], 'id="s3_bucket"').m62_form_errors($form_errors['s3_bucket'])
-);
-
-$this->table->add_row(
-    '<label for="s3_optional_prefix">'.$view_helper->m62Lang('s3_optional_prefix').'</label><div class="subtext">'.$view_helper->m62Lang('s3_optional_prefix_instructions').'</div>',
-    form_input('s3_optional_prefix', $form_data['s3_optional_prefix'], 'id="s3_bucket"').m62_form_errors($form_errors['s3_optional_prefix'])
-);
-
-$this->table->add_row(
-    '<label for="s3_reduced_redundancy">'.$view_helper->m62Lang('s3_reduced_redundancy').'</label><div class="subtext">'.$view_helper->m62Lang('s3_reduced_redundancy_instructions').'</div>', 
-    form_checkbox('s3_reduced_redundancy', '1', $form_data['s3_reduced_redundancy'], 'id="s3_reduced_redundancy"').m62_form_errors($form_errors['s3_reduced_redundancy'])
-);
+<tr>
+    <th scope="row">
+        <label for="s3_access_key"><?php echo $view_helper->m62Lang('s3_access_key'); ?></label>
+    </th>
+    <td>
+        <input name="s3_access_key" type="text" id="s3_access_key" value="<?php echo $form_data['s3_access_key']; ?>" class="regular-text code" />
+        <p class="description" id="s3_access_key-description"><?php echo $view_helper->m62Lang('s3_access_key_instructions'); ?></p>
+        <?php echo $this->backup_lib->displayFormErrors($form_errors['s3_access_key']); ?>
+    </td>
+</tr>
+<tr>
+    <th scope="row">
+        <label for="s3_secret_key"><?php echo $view_helper->m62Lang('s3_secret_key'); ?></label>
+    </th>
+    <td>
+        <input name="s3_secret_key" type="password" id="s3_secret_key" value="<?php echo $form_data['s3_secret_key']; ?>" class="regular-text code" />
+        <p class="description" id="rcf_api-description"><?php echo $view_helper->m62Lang('s3_secret_key_instructions'); ?></p>
+        <?php echo $this->backup_lib->displayFormErrors($form_errors['s3_secret_key']); ?>
+    </td>
+</tr>
+<tr>
+    <th scope="row">
+        <label for="s3_bucket"><?php echo $view_helper->m62Lang('s3_bucket'); ?></label>
+    </th>
+    <td>
+        <input name="s3_bucket" type="text" id="s3_bucket" value="<?php echo $form_data['s3_bucket']; ?>" class="regular-text code" />
+        <p class="description" id="s3_bucket-description"><?php echo $view_helper->m62Lang('s3_bucket_instructions'); ?></p>
+        <?php echo $this->backup_lib->displayFormErrors($form_errors['s3_bucket']); ?>
+    </td>
+</tr>
+<tr>
+    <th scope="row">
+        <label for="s3_optional_prefix"><?php echo $view_helper->m62Lang('s3_optional_prefix'); ?></label>
+    </th>
+    <td>
+        <input name="s3_optional_prefix" type="text" id="s3_optional_prefix" value="<?php echo $form_data['s3_optional_prefix']; ?>" class="regular-text code" />
+        <p class="description" id="s3_optional_prefix-description"><?php echo $view_helper->m62Lang('s3_optional_prefix_instructions'); ?></p>
+        <?php echo $this->backup_lib->displayFormErrors($form_errors['s3_optional_prefix']); ?>
+    </td>
+</tr>
+<tr>
+    <th scope="row">
+        <label for="s3_reduced_redundancy"><?php echo $view_helper->m62Lang('s3_reduced_redundancy'); ?></label>
+    </th>
+    <td>
+        <fieldset><legend class="screen-reader-text"><span><?php echo $view_helper->m62Lang('s3_reduced_redundancy_instructions'); ?></span></legend><label for="s3_reduced_redundancy">
+            <input name="s3_reduced_redundancy" id="s3_reduced_redundancy" value="1" type="checkbox" <?php echo checked( $form_data['s3_reduced_redundancy'], 1, true); ?>>
+            <?php echo $view_helper->m62Lang('s3_reduced_redundancy_instructions'); ?></label></fieldset>
+        <?php echo $this->backup_lib->displayFormErrors($form_errors['s3_reduced_redundancy']); ?>
+    </td>
+</tr>
