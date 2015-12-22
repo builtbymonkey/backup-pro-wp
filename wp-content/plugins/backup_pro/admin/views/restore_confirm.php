b3af9720293eb74b4b7b94a384ea973a641aa16c
Eric Lamb <eric@mithra62.com>
2015-12-21 17:02:46 -0800

    Adds table layout to database restore confirm

--- wp-content/plugins/backup_pro/admin/views/restore_confirm.php
+++ wp-content/plugins/backup_pro/admin/views/restore_confirm.php
@@ -6,23 +6,51 @@
 <h2><?php echo $view_helper->m62Lang('restore_db'); ?></h2>
 
 <p><?php echo $view_helper->m62Lang('restore_db_question'); ?></p>
-<p class="notice"><?php echo $view_helper->m62Lang('action_can_not_be_undone'); ?> <?php echo $view_helper->m62Lang('restore_double_speak'); ?></p>
+<p style="color:red"><?php echo $view_helper->m62Lang('action_can_not_be_undone'); ?> <?php echo $view_helper->m62Lang('restore_double_speak'); ?></p>
 
-<p>
-	<strong><?php echo $view_helper->m62Lang('taken'); ?>:</strong> <?php echo $view_helper->m62DateTime($backup['created_date']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('backup_type'); ?>:</strong> <?php echo $view_helper->m62Lang($backup['database_backup_type']); ?><br />
-	<strong><?php echo $view_helper->m62Lang('verified'); ?>:</strong> 
-		<?php if( $backup['verified'] == '1'): ?>
+	<table border="0" cellspacing="0" cellpadding="0" class="widefat"  width="100%" >
+
+	<tbody>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('taken'); ?>:</strong></td>
+		<td><?php echo $view_helper->m62DateTime($backup['created_date']); ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('backup_type'); ?>:</strong></td>
+		<td><?php echo $view_helper->m62Lang($backup['database_backup_type']); ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('verified'); ?>:</strong></td>
+		<td>
+		<?php if( $backup['verified'] === 'success'): ?>
 			<span class=""><?php echo $view_helper->m62Lang('yes'); ?></span>
 		<?php else: ?>
-			<span class="notice"><?php echo $view_helper->m62Lang('no'); ?></span>
-		<?php endif; ?> <br />
-	<strong><?php echo $view_helper->m62Lang('time_taken'); ?>:</strong> <?php echo number_format($backup['time_taken'], 2, '.', ','); ?>s <br />
-	<strong><?php echo $view_helper->m62Lang('max_memory'); ?>:</strong> <?php echo $view_helper->m62FileSize($backup['max_memory']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('uncompressed_sql_size'); ?>:</strong> <?php echo $view_helper->m62FileSize($backup['uncompressed_size']); ?> <br />
-	<strong><?php echo $view_helper->m62Lang('total_tables'); ?>:</strong> <?php echo $backup['item_count']; ?><br />
-	<strong><?php echo $view_helper->m62Lang('md5_hash'); ?>:</strong> <?php echo $backup['hash']; ?>
-</p>
+			<span style="color:red"><?php echo $view_helper->m62Lang('no'); ?></span>
+		<?php endif; ?>		
+		</td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('time_taken'); ?>:</strong></td>
+		<td><?php echo $view_helper->m62TimeFormat($backup['time_taken']); ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('max_memory'); ?>:</strong></td>
+		<td><?php echo $view_helper->m62FileSize($backup['max_memory']); ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('uncompressed_sql_size'); ?>:</strong></td>
+		<td><?php echo $view_helper->m62FileSize($backup['uncompressed_size']); ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('total_tables'); ?>:</strong></td>
+		<td><?php echo $backup['item_count']; ?></td>
+	</tr>
+	<tr>
+		<td><strong><?php echo $view_helper->m62Lang('md5_hash'); ?>:</strong></td>
+		<td><?php echo $backup['hash']; ?></td>
+	</tr>
+	</tbody>
+	</table>
 
 <?php //echo form_open($query_base.'restore_database&id='.urlencode($view_helper->m62Encode($backup['details_file_name'])), array('id'=>'backup_form')); ?>
     <form action="<?php echo $url_base; ?>restore_database&id=<?php echo urlencode($view_helper->m62Encode($backup['details_file_name'])); ?>" method="post">
